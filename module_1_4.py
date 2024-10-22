3ea72bf8c2a475c720860fdb5366181ff59a6670
DmitryPerov <dmitryperov73@gmail.com>
2024-10-22 23:35:58 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,14 @@
+my_string = input("Введите строку с произвольным текстом ")
+print (my_string.count('',0)-1)
+print (my_string.upper())
+print (my_string.lower())
+print (my_string.replace(" ",''))
+print (my_string[0])
+print (my_string[-1])
+
+
+
+
+
+
+
